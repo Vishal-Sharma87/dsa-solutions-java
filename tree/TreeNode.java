f5e3b3baaef898350c459ba62eaf0ffeb5af4017
Vishal Sharma <vishalimp03@gmail.com>
2026-05-07 00:02:49 +0530

    feat: add TreeNode class for binary tree node representation

---
+++ tree/TreeNode.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int d) {
+        val = d;
+        left = null;
+        right = null;
+    }
+}
